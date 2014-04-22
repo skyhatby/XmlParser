4f1d53fe2048ea264555e0e106210bee85191878
skyhat <skyhat@mail.ru>
2014-04-22 11:32:26 +0300

    Initial Commit

---
+++ XmlParser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlParser
+{
+    public class Class1
+    {
+    }
+}
